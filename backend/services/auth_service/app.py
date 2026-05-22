90896152188008c033d1565eaf58f79c35e5a79d
Nitish Kumar <nitish88631006@gmail.com>
2026-05-23 01:33:36 +0530

    🧑‍🍳 added prometheus

--- backend/services/auth_service/app.py
+++ backend/services/auth_service/app.py
@@ -1,21 +1,44 @@
 from fastapi import FastAPI
 import random
 import time
-from prometheus_client import Counter, generate_latest
+from prometheus_client import Counter, generate_latest, Histogram
 from fastapi.responses import Response
 from loguru import logger
 
 app = FastAPI()
 
 REQUEST_COUNT = Counter("request_count", "Total Request Count")
+ERROR_COUNT = Counter(
+    "login_errors_total","Total login requests"
+)
+LATENCY = Histogram("login_latency_seconds", "Login latency")
+
+
 
 
 @app.get("/login")
 def login():
-    if random.randint(1,10) > 7:
-        return {"error: db timeout"}
-        logger.error("database timeout")
+    REQUEST_COUNT.inc()
+    start = time.time()
+    dice = random.randint(1,10)
+    logger.info("Login request received")
+    #simulate slow db
+    if dice > 7:
+        logger.warning("Database responding slowly")
+        time.sleep(3)
+
+    if dice > 8:
+        ERROR_COUNT.inc()
+        logger.error("database timeout occurred:")
+
+        LATENCY.observe(time.time() - start)
+
+        return {
+            "status" : "error",
+            "message": "Database timeout"
+        }
     
+    LATENCY.observe(time.time() - start)
     return {"status":"success"}
 
 
